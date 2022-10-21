ac9c3138cbc7e06f46a15466c3c3b588e1881419
Palina <114215521+Palina-frog-dog-whatelse@users.noreply.github.com>
2022-10-21 09:40:26 +0300

    Update 2_2.py

--- 2_2.py
+++ 2_2.py
@@ -1,9 +1,11 @@
-n, m = map(int, input().split())
-ans = []
-a = [int(i) for i in input().split()]
-b = [int(i) for i in input().split()]
+def sr6():
+    n, m = map(int, input().split())
+    ans = []
+    a = [int(i) for i in input().split()]
+    b = [int(i) for i in input().split()]
 
-for i in a:
-    if i in b and i not in ans:
-        ans.append(i)
-print(ans)
+    for i in a:
+        if i in b and i not in ans:
+            ans.append(i)
+    print(ans)
+sr6()
